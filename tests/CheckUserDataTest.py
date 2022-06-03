e91af1270e690e7ae071f25d292c01d4d0182531
CarolinaMargiotti <55335180+CarolinaMargiotti@users.noreply.github.com>
2022-06-03 08:12:42 -0300

    adicionado "test" no começo das funções

--- tests/CheckUserDataTest.py
+++ tests/CheckUserDataTest.py
@@ -3,11 +3,11 @@ from controllers.adm_controller import checkUserDataIsValid
 from unittest import TestCase
 
 class CheckUserDataTest(TestCase):
-    def shouldReturnStatusCode200IfDataCorrect(self):
+    def test_shouldReturnStatusCode200IfDataCorrect(self):
         res = checkUserDataIsValid("user","password")
         self.assertEqual(res.status_code, 200)
 
-    def shouldReturnRightStatusCodeAndMessageIfUserEmpty(self):
+    def test_shouldReturnRightStatusCodeAndMessageIfUserEmpty(self):
         res = checkUserDataIsValid("","password")
         res2 = checkUserDataIsValid(None,"password")
         self.assertEqual(res.status_code, 400)
@@ -15,7 +15,7 @@ class CheckUserDataTest(TestCase):
         self.assertEqual(res2.status_code, 400)
         self.assertEqual(res2.json.mensagem, '')
 
-    def shouldReturnRightStatusCodeAndMessageIfPasswordEmpty(self):
+    def test_shouldReturnRightStatusCodeAndMessageIfPasswordEmpty(self):
         res = checkUserDataIsValid("user","")
         res2 = checkUserDataIsValid("user",None)
         self.assertEqual(res.status_code, 400)
